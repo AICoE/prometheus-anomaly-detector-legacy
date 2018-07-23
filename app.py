eb7cfe4c200613d3270f972e61b761ed68577826
Anand Sanmukhani <asanmukhredhat.com>
2018-07-23 12:54:49 -0400

    Remove loop to train model for all sub labels

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lib.ceph import CephConnect as cp
 
 from fbprophet import Prophet
 import os
+import gc
 
 
 # def get_df_from_json(metric):
@@ -40,7 +41,6 @@ import os
 #     return metric_dict_pd
 
 def get_df_from_json(metric):
-    print("Here")
     '''
     Method to convert a json object of a Prometheus metric to a dictionary of shaped Pandas DataFrames
 
@@ -85,6 +85,7 @@ print("Using Metric {}.".format(metric_name))
 
 metric = prom.get_metric(metric_name)
 
+del prom
 
 metric = json.loads(metric)
 
@@ -94,53 +95,68 @@ print("----------------------------------\n")
 
 # Metric Json is converted to a shaped dataframe
 new_dict = get_df_from_json(metric)
-# del metric
-print(len(new_dict))
-for key in new_dict:
-    print("----------------------------------\n")
-    print(key)
-    print("----------------------------------\n")
-    # print(new_dict[key])
-    # data_pd = new_dict[key]
-    train_frame = new_dict[key][0 : int(0.7*len(new_dict[key]))]
-    test_frame = new_dict[key][int(0.7*len(new_dict[key])) : ]
 
-    print(len(train_frame), len(test_frame), len(new_dict[key]))
-    print(train_frame.head())
-    print(test_frame.head())
-    print(new_dict[key].head())
-    train_frame['y'] = train_frame['value']
-    train_frame['ds'] = train_frame['timestamp']
+del metric
 
-    m = Prophet()
+for key, value in new_dict.items():
+    meta_data = key
+    data = value
 
-    m.fit(train_frame)
-
-    future = m.make_future_dataframe(periods= int(len(test_frame) * 1.1),freq= '1MIN')
-    forecast = m.predict(future)
-    print(forecast.head())
-
-    forecasted_features = ['ds','yhat','yhat_lower','yhat_upper']
-    # m.plot(forecast,xlabel="Time stamp",ylabel="Value");
-    # m.plot_components(forecast);
-
-    forecast = forecast[forecasted_features]
-    print(forecast.head())
-
-    forecast['timestamp'] = forecast['ds']
-    forecast['values'] = data_pd['values']
-    forecast = forecast[['timestamp','values','yhat','yhat_lower','yhat_upper']]
-    output_json = key + forecast.to_json()
-
-    # Store Forecast to CEPH
-    session = cp()
-    object_path = "Predictions" + "/" + prom_host + "/" + url + "/" + "Predictions" + ".json"
-    print(session.store_data(name = metric_name, object_path = object_path, values = forecast.to_json()))
-    # file = open('Predictions.json', 'w')
-    # file.write(output_json)
-    # file.close()
-    # forecast.to_json()
     break
+    pass
+print(meta_data, data)
+
+del new_dict
+# print(len(new_dict))
+# for key in new_dict:
+key = meta_data
+print("----------------------------------\n")
+print(key)
+print("----------------------------------\n")
+# print(new_dict[key])
+# data_pd = new_dict[key]
+train_frame = data[0 : int(0.7*len(data))]
+test_frame = data[int(0.7*len(data)) : ]
+
+print(len(train_frame), len(test_frame), len(data))
+print(train_frame.head())
+print(test_frame.head())
+print(data.head())
+train_frame['y'] = train_frame['value']
+train_frame['ds'] = train_frame['timestamp']
+
+m = Prophet()
+print("Fitting the train_frame")
+m.fit(train_frame)
+
+future = m.make_future_dataframe(periods= int(len(test_frame) * 1.1),freq= '1MIN')
+print("----------------------------------\n")
+gc.collect()
+print("Predicting Future.................")
+forecast = m.predict(future)
+print(forecast.head())
+
+forecasted_features = ['ds','yhat','yhat_lower','yhat_upper']
+# m.plot(forecast,xlabel="Time stamp",ylabel="Value");
+# m.plot_components(forecast);
+
+forecast = forecast[forecasted_features]
+print(forecast.head())
+
+forecast['timestamp'] = forecast['ds']
+forecast['values'] = data_pd['values']
+forecast = forecast[['timestamp','values','yhat','yhat_lower','yhat_upper']]
+output_json = key + forecast.to_json()
+
+# Store Forecast to CEPH
+session = cp()
+object_path = "Predictions" + "/" + prom_host + "/" + url + "/" + "Predictions" + ".json"
+print(session.store_data(name = metric_name, object_path = object_path, values = forecast.to_json()))
+# file = open('Predictions.json', 'w')
+# file.write(output_json)
+# file.close()
+# forecast.to_json()
+# break
 
 
 #     print(len(metric_dict[key]))
